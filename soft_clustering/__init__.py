9b7b4fdf2a72a56b4e42ab192bb972f74328a313
MG-530 <mg1383530@gmail.com>
2026-06-02 20:27:58 +0330

    refactor(package): expose all clustering algorithms

--- soft_clustering/__init__.py
+++ soft_clustering/__init__.py
@@ -1,40 +1,83 @@
-"""Configure global settings and get information about the working environment."""
-
-__version__ = "0.0.1"
-
-from ._nocd import NOCD
-from ._wbsc import WBSC
-from ._pfcm import PFCM
-from ._rough_k_means import RoughKMeans
+from ._afcm import AFCM
+from ._afcmadaptive import AFCMAdaptive
+from ._afcmSimple import AFCMSimple
+from ._bgmm import BGMM
+from ._bigclam import BIGCLAM
+from ._bnmf import BNMF
+from ._cafcm import CAFCM
+from ._cafhfcm import CAFHFCM
+from ._cdcgs import CDCGS
+from ._dmon import DMoN
+from ._ecm import ECM
+from ._entropyfcm import ENTROPYFCM
+from ._fcc import FCC
+from ._fcm import FuzzyCMeans as FCM
+from ._femifuzzy import FeMIFuzzy
+from ._gk import GustafsonKessel as GK
+from ._gmm import GaussianMixtureEM as GMM
+from ._kfccl import KFCCL
+from ._kfcm import KFCM
+from ._kmart import KMART
 from ._lda import LDA
+from ._mbmm import MBMM
+from ._mmsb import MMSB
+from ._nocd import NOCD
+from ._pcm import PossibilisticCMeans as PCM
+from ._pfcm import PFCM
 from ._plsi import PLSI
 from ._rdfkc import RDFKC
-from ._kfcm import KFCM
-from ._kfccl import KFCCL
+from ._rough_k_means import RoughKMeans
+from ._rpfkm import RPFKM
 from ._scm import SCM
-from ._fcm import FuzzyCMeans as FCM
-from ._gmm import GaussianMixtureEM as GMM
-from ._pcm import PossibilisticCMeans as PCM
-from ._gk import GustafsonKessel as GK
+from ._scspa import SCSPA
+from ._sfcmep import SFCMEP
+from ._shbgf import SHBGF
 from ._sisc import SISC
-from ._kmart import KMART
-
+from ._skfcm import SKFCM
+from ._smcla import SMCLA
+from ._softdbscangm import SoftDBSCANGM
+from ._softksc import SoftKSC
+from ._wbsc import WBSC
 
 __all__ = [
-    "NOCD",
-    "WBSC",
-    "PFCM",
-    "RoughKMeans",
+    "AFCM",
+    "AFCMAdaptive",
+    "AFCMSimple",
+    "BGMM",
+    "BIGCLAM",
+    "BNMF",
+    "CAFCM",
+    "CAFHFCM",
+    "CDCGS",
+    "DMoN",
+    "ECM",
+    "ENTROPYFCM",
+    "FCC",
+    "FCM",
+    "FeMIFuzzy",
+    "GK",
+    "GMM",
+    "KFCCL",
+    "KFCM",
+    "KMART",
     "LDA",
+    "MBMM",
+    "MMSB",
+    "NOCD",
+    "PCM",
+    "PFCM",
     "PLSI",
     "RDFKC",
-    "KFCM",
-    "KFCCL",
+    "RoughKMeans",
+    "RPFKM",
     "SCM",
-    "FCM",
-    "GMM",
-    "PCM",
-    "GK",
+    "SCSPA",
+    "SFCMEP",
+    "SHBGF",
     "SISC",
-    "KMART",
+    "SKFCM",
+    "SMCLA",
+    "SoftDBSCANGM",
+    "SoftKSC",
+    "WBSC",
 ]
