71f540e8be00d4f16286bc0e8a5896be0c144fdf
Morteza-24 <mortezia4444.mz@gmail.com>
2026-07-23 19:22:32 +0330

    bump version to update pypi

--- soft_clustering/__init__.py
+++ soft_clustering/__init__.py
@@ -1,6 +1,6 @@
 """Soft Clustering - A library of soft and fuzzy clustering algorithms."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from ._afcm import AFCM
 from ._afcmadaptive import AFCMAdaptive
